45699b1e0225f5fce161acf96b1524fac118780e
emiliawcn <emilia@gasslander.com>
2024-12-19 15:02:59 +0100

    idfk
    har lafgt till dots som gör att images ändrar, ligger inte i obj2 och imgs är inte på rätt ställe

--- animation.js
+++ animation.js
@@ -1,30 +1,40 @@
 const scrollDownButton = document.getElementById('scrollDownButton');
 
-// Make sure the button is visible
+
 scrollDownButton.style.display = "block";
 
-// Add a click event listener to scroll to the desired section
+
 scrollDownButton.addEventListener('click', function () {
     document.querySelector(".obj3").scrollIntoView({
         behavior: 'smooth'
     });
 });
 
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// Array of image URLs
-const images = [
-  "http://31.media.tumblr.com/18b5f8f0a00ad01e50f7ae2f513be52d/tumblr_msqcl4iwM01soh1p8o1_500.png",
-  "http://31.media.tumblr.com/fca646cd8fe87906e605ad7e8d039903/tumblr_mmoz4fWT6U1soh1p8o1_500.png"
-];
-
-let currentIndex = 0; // Tracks the current image index
-
-// Function to change the image
-function changeImage(direction) {
-  currentIndex = (currentIndex + direction + images.length) % images.length; // Update index
-  document.getElementById("theImage").src = images[currentIndex]; // Update image source
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-// Add event listeners to buttons
-document.getElementById("arrow-one").addEventListener("click", () => changeImage(-1)); // Left arrow
-document.getElementById("arrow-two").addEventListener("click", () => changeImage(1));  // Right arrow
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
